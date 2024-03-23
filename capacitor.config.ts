782565bbe47e621b2c7690c69757913c55eec095
rishi-95 <rishisiliveri243@gmail.com>
2024-03-23 22:14:28 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Babuji Sahaj Marg',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
